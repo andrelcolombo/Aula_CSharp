377d5c036aa5f446907b4b6acbb788edbacf223f
André Luiz Colombo <andreluizcolombo@gmail.com>
2024-03-29 16:42:33 -0300

    Aula04

--- Fundamentos.CSharp.Cap03.POO/Program.cs
+++ Fundamentos.CSharp.Cap03.POO/Program.cs
@@ -1,15 +1,46 @@
 ﻿using Fundamentos.CSharp.Cap03.POO;
 
-Automovel carro = new Automovel();
+//Automovel carro = new Automovel();
 
-carro.Fabricante = "VolksWagen";
-carro.Modelo = "GOL";
+//carro.Fabricante = "VolksWagen";
+//carro.Modelo = "GOL";
 //carro.Placa = "QUO7H18";
 //carro.Cor = "Branco";
 //carro.Ano = 2020;
 
-Console.WriteLine("Fabricante: " + carro.Fabricante);
-Console.WriteLine("Modelo: " + carro.Modelo);
+//Console.WriteLine("Fabricante: " + carro.Fabricante);
+//Console.WriteLine("Modelo: " + carro.Modelo);
 //Console.WriteLine("Placa: " + carro.Placa);
 //Console.WriteLine("Cor: " + carro.Cor);
 //Console.WriteLine("Ano: " + carro.Ano);
+
+//Colaborador colab = new Colaborador();
+
+//colab.Nome = "Jimmy Page";
+//colab.Cargo = "Toca muito";
+//colab.Salario = 39.555;
+
+//Console.WriteLine("Nome do colaborador(a): " + colab.Nome);
+//Console.WriteLine("Cargo: " + colab.Cargo);
+//Console.WriteLine("Salario " + colab.Salario);
+//Console.WriteLine("IRPF: " + colab.Irpf);
+
+//Colaborador colab = new Colaborador();
+
+//colab.Atribuir("BillGates", "Estagiario", 680.35);
+
+
+//TesteEnum enumeradora = new TesteEnum();
+
+//enumeradora.Percorrer();
+
+
+//Automovel.Fabricante = "VolksWagen";
+
+//Automovel.Modelo = "Gol";
+
+//Console.WriteLine("Fabrincante: " + Automovel.Fabricante);
+//Console.WriteLine("Modelo: " + Automovel.Modelo);
+
+
+Console.WriteLine(OutroElemStatic.Multiplicar(12, 5));
---
+++ Fundamentos.CSharp.Cap03.POO/TesteEnum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos.CSharp.Cap03.POO
+{
+    internal class TesteEnum
+    {
+        enum EstadosCivis : byte
+        { 
+            casado,
+            solteiro,
+            Viuvo,
+            Divorciado,
+            Outros
+        }
+
+        public void Percorrer()
+        {
+            foreach (byte i in Enum.GetValues(typeof(EstadosCivis)))
+            {
+                Console.WriteLine(Enum.GetName(typeof(EstadosCivis), i));
+            }
+        }
+    }
+}
